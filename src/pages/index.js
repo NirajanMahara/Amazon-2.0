1d794893b4c0ddf4d7390e9c9b472896d75fabbb
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2021-12-11 19:52:00 +0545

    Building the Banner Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
       <Header />
 
       <main className="max-w-screen-2xl mx-auto">
-        {/* Banner */}
+        <Banner />
 
         {/* ProductFeed */}
       </main>
