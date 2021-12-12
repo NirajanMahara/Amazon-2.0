16aa8b2882ca6ca4edd6abc9f4e9928959184d6c
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2021-12-12 13:25:01 +0545

    Implementing Firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDZN7_RXG_6W2ZUJEgbx9SuRleNjUHoss0",
