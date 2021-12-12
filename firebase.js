ce2b608fad3411da6bf7b3e3853bb4e8c6bb3368
Nirajan Mahara <N.Mahara@wlv.ac.uk>
2021-12-12 13:23:01 +0545

    Firebase `Config`

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZN7_RXG_6W2ZUJEgbx9SuRleNjUHoss0",
+  authDomain: "amzn-2021.firebaseapp.com",
+  projectId: "amzn-2021",
+  storageBucket: "amzn-2021.appspot.com",
+  messagingSenderId: "209595993690",
+  appId: "1:209595993690:web:ad5f8e06eec2feb4009263",
+  measurementId: "G-QGCG0XXZG1",
+};
